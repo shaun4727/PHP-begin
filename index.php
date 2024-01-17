bee37e23b79dcf9b9afdc8c8e91c475f59778c35
shaun4727 <107396795+shaun4727@users.noreply.github.com>
2024-01-17 19:25:22 +0600

    database connection made with pdo

--- index.php
+++ index.php
@@ -2,6 +2,20 @@
 
 require 'functions.php';
 
-require 'router.php';
+// require 'router.php';
+
+
+$dsn = "mysql:host=localhost;port=3306;user=root;dbname=ecommerce;charset=utf8";
+
+$pdo = new PDO($dsn,'root');
+// $pdo = new PDO($dsn,'root');
+
+$statement = $pdo->prepare("select * from brands");
+
+$statement->execute();
+
+$brands = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($brands);
 
 
