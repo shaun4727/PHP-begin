af69b7a12e6cc63083f1f2adda964a9ebdf6c412
shaun4727 <107396795+shaun4727@users.noreply.github.com>
2024-01-21 22:22:06 +0600

    autoloader added

--- public/index.php
+++ public/index.php
@@ -4,8 +4,11 @@ const BASE_PATH = __DIR__.'/../';
 
 require BASE_PATH.'functions.php';
 
-base_path('Response.php');
-base_path('Database.php');
+
+spl_autoload_register(function($class){
+    base_path("core/{$class}.php");
+});
+
 base_path('router.php');
 
 
